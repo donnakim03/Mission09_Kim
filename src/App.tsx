72c1730a44b8ccaa3f1311b0b7bfc33d6dd678e1
donnakim03 <dkim210@byu.edu>
2024-03-08 13:59:25 -0700

    team list complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import collegeBasketballData from './CollegeBasketballTeams.json';
+
+interface TeamProps {
+  tid: number;
+  cid: number;
+  did: number;
+  school: string;
+  name: string;
+  abbrev: string;
+  pop: number;
+  city: string;
+  state: string;
+  latitude: number;
+  longitude: number;
+}
 
 const basketballData = collegeBasketballData.teams;
 
@@ -19,10 +32,37 @@ function SiteIntro() {
   );
 }
 
+class Team extends React.Component<TeamProps> {
+  render() {
+    const oneTeam = this.props;
+
+    return (
+      <div className="card">
+        <h4>School: {oneTeam.school}</h4>
+        <h4>Mascot: {oneTeam.name}</h4>
+        <h4>
+          Location: {oneTeam.city}, {oneTeam.state}
+        </h4>
+      </div>
+    );
+  }
+}
+
+function TeamList() {
+  return (
+    <div>
+      {basketballData.map((teamNum) => (
+        <Team {...teamNum} />
+      ))}
+    </div>
+  );
+}
+
 function App() {
   return (
     <div className="App">
       <SiteIntro />
+      <TeamList />
     </div>
   );
 }
