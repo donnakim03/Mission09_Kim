259d8cf99c3ed0f01d6a3144c6d48262b734185f
donnakim03 <dkim210@byu.edu>
2024-03-08 13:30:59 -0700

    site intro function implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,28 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import collegeBasketballData from "./CollegeBasketballTeams.json";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import collegeBasketballData from './CollegeBasketballTeams.json';
 
 const basketballData = collegeBasketballData.teams;
+
+function SiteIntro() {
+  return (
+    <div>
+      <h1>
+        This site shows information about all the colleges in NCAA Basketball.
+      </h1>
+      <h2>
+        You can look through the school name, mascot name, and location of all
+        the teams.
+      </h2>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SiteIntro />
     </div>
   );
 }
